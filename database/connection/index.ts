19952954e2e430d6b8beb5c2e97e0b7a64120ca6
danielalexis <danielalexis.o.pereira@gmail.com>
2022-11-01 20:17:45 +0000

    remove type from function and fix test

--- database/connection/index.ts
+++ database/connection/index.ts
@@ -1,7 +1,7 @@
-import { Db, MongoClient } from "mongodb";
+import { MongoClient } from "mongodb";
 import details from "./details.json" assert {"type": "json"};
 
-async function generateConnection(): Promise<MongoClient|Db> {
+async function generateConnection() {
     const client = new MongoClient(details.uri);
     await client.connect();
     return client.db(details.db);
--- test/test1.ts
+++ test/test1.ts
@@ -3,7 +3,7 @@ import * as conn from "../database/connection/index.js";
 console.log("Test 1: Record insertion");
 try {
     const client = await conn.generateConnection();
-    const insertRecord = await conn.setDb(client).collection("user").insertOne({"a": "a"});
+    const insertRecord = await client.collection("user").insertOne({"a": "a"});
     console.log(insertRecord);
     process.exit();
 } catch (error) {
