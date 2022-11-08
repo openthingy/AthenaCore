1ec5b622d35035a186935df683c11f4479edf4fb
danielalexis <danielalexis.o.pereira@gmail.com>
2022-11-08 17:56:24 +0000

    Work 1

---
+++ modules/inventory/warehouse.ts
@@ -0,0 +1,46 @@
+import { InsertOneResult } from "mongodb";
+import * as db from "../../database/index.js";
+import { WarehouseInfo } from "../interfaces/inventory/warehouse.js";
+
+class warehouse {
+    public async createWarehouse(warehouse: WarehouseInfo): Promise<boolean> {
+        try {
+            const client = await db.generateConnection();
+            const warehouses: number = await client.collection("warehouses").countDocuments({"_id": warehouse._id});
+            if (warehouses != 0) {
+                const createWarehouse: InsertOneResult<Document> = await client.collection("warehouses").insertOne(warehouse as object);
+                if (createWarehouse.acknowledged) {
+                    return true;
+                } else {
+                    throw new Error("Something went wrong while inserting a warehouse");
+                }
+            }
+        }
+        catch (err) {
+            console.log("Error: " + err);
+            return false;
+        }
+        // You shouldn't be getting here
+        return false;
+    }
+
+    public async getWarehouse(code: WarehouseInfo["_id"]) {
+        try {
+            const client = await db.generateConnection();
+            const warehouses: number = await client.collection("warehouses").countDocuments({"_id": code});
+            if (warehouses > 0) {
+                const warehouse = await client.collection("warehouses").findOne({"_id": code} as object);
+                return warehouse;
+            } else {
+                return false;
+            }
+        }
+        catch (err) {
+            console.log("Error: " + err);
+            return false;
+        }
+    }
+}
+
+
+export { warehouse };
