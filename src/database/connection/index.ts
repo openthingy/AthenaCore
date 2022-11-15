f9bcb1f4a0477d1e1b9cb199ee7f9524a7476134
danielalexis <danielalexis.o.pereira@gmail.com>
2022-11-15 15:27:36 +0000

    add db selection so connection close is possible

--- src/database/connection/index.ts
+++ src/database/connection/index.ts
@@ -1,12 +1,17 @@
 import { MongoClient, Db } from "mongodb";
 import details from "./details.json";
 
-async function generateConnection(): Promise<Db> {
+async function generateConnection(): Promise<MongoClient> {
     const client = new MongoClient(details.uri);
+    return client;
+}
+
+async function selectDb(client: MongoClient): Promise<Db> {
     await client.connect();
     return client.db(details.db);
 }
 
 export { 
-    generateConnection
+    generateConnection,
+    selectDb
 };
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,4 +1,4 @@
-import { generateConnection } from "./connection/index.js";
+import { generateConnection, selectDb } from "./connection/index.js";
 import { sanitizeInput } from "./protection/index.js";
 
-export { generateConnection, sanitizeInput };
+export { generateConnection, selectDb, sanitizeInput };
