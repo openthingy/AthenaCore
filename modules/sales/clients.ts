85a522ab9ea4cfbddb341cb1045c9e6596e26d18
danielalexis <danielalexis.o.pereira@gmail.com>
2022-11-15 09:42:51 +0000

    a

--- modules/sales/clients.ts
+++ modules/sales/clients.ts
@@ -1,8 +1,24 @@
 import { Client } from "../interfaces/client.js";
+import * as db from "../../database/index.js";
+import { ObjectId, WithId } from "mongodb";
 
 class clients {
-    public static getClientByID(): Client | null {
-        return null;
+    public static async createClient() {
+        console.log("a");
+    }
+
+    public static async getClient(id?: Client["_id"], name?:  Client["name"], vatId?: Client["vatId"]) {
+        if (typeof id != undefined) {
+            this.getClientByID(id);
+        }
+        console.log("a");
+    }
+
+    private static async getClientByID(clientId: Client["_id"]): Promise<WithId<Document | null > | null> {
+        const client = await db.generateConnection();
+        const insertRecord = await client.collection("clients").findOne({"_id": clientId});
+        console.log(insertRecord);
+        return insertRecord;
     }
 }
 
