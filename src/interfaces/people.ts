6fb5045a7a9efccf2d33f1784d080ef2dd5b08a7
danielalexis <danielalexis.o.pereira@gmail.com>
2022-12-25 22:14:27 +0000

    Update employee interface to have the title

--- src/interfaces/people.ts
+++ src/interfaces/people.ts
@@ -7,8 +7,9 @@ interface Employee {
     address: Address,
     vatId: string,
     ssn: string,
+    title: string,
     email: string,
-    phoneNumber: string,
+    phoneNumber: string | null, // Not required as not all employees have a company phone number
     password: string
     roles: Array<string>
 }
